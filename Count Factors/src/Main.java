7ebf573016efc98cc2057ee0254ba0a88f06b196
Ace <152076508+AkshayDilipYadav@users.noreply.github.com>
2024-05-01 00:37:47 +0530

    Add files via upload

--- Count Factors/src/Main.java
+++ Count Factors/src/Main.java
@@ -4,18 +4,30 @@ public class Main {
 Scanner sc = new Scanner (System.in);
 int A = sc.nextInt();
 
-int countFactors = countFactor(A);
-System.out.println(countFactors);
+
+
+int[] result = countFactor(A);
+int count = result[0];
+int runs = result[1];
+        System.out.println( "Runs:"+runs);
+System.out.println(count);
+
     }
 
-    public static int countFactor(int A){
+    public static int[] countFactor(int A){
+        int[] result = new int[2];
+
+        int runs=0;
         int count =0;
         for (int i = 1; i <= A; i++){
             if (A%i == 0){
                 count++;
             }
+            runs++;
         }
-        return count;
+        result[0] = count;
+        result[1] = runs;
+        return result;
     }
 
 }
