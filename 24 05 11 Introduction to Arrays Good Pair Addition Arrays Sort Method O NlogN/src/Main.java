758615a5a3f1b80d369c7eee4e86f777b7b72d5a
Ace <152076508+AkshayDilipYadav@users.noreply.github.com>
2024-05-11 10:37:32 +0530

    Add files via upload

---
+++ 24 05 11 Introduction to Arrays Good Pair Addition Arrays Sort Method O NlogN/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Main {
+    public static void main (String[] args){
+        Scanner sc = new Scanner (System.in);
+        int N = sc.nextInt();
+        int[] A = new int[N];
+        for (int i = 0; i < A.length; i++){
+            A[i] = sc.nextInt();
+        }
+        int B = sc.nextInt();
+       System.out.print( goodPair(A, B));
+    }
+    public static int goodPair (int[] A, int B){
+        int l = 0;
+        int r = A.length -1;
+
+        while (l<r){
+            int sum = l+r;
+            if (sum == B){
+                return 1;
+
+            }
+            else if (sum < B){l++;}
+            else {r--;}
+        }
+        return 0;
+    }
+}
