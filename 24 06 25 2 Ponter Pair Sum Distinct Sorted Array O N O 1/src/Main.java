7d9ca73651e9e66ab47b11974db1b1cafedbc144
Ace <152076508+AkshayDilipYadav@users.noreply.github.com>
2024-06-25 00:30:27 +0530

    Add files via upload

---
+++ 24 06 25 2 Ponter Pair Sum Distinct Sorted Array O N O 1/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for(int i =0; i< n; i++){
+            a[i] = sc.nextInt();
+        }
+        int k = sc.nextInt();
+        if(hasPairSum(a, k)){System.out.print("True");}
+        else{System.out.print("True");}
+    }
+    public static boolean hasPairSum(int[] a, int k){
+        int left = 0;
+        int right = a.length -1;
+        while(left< right){
+            int sum = a[left ] + a[right];
+            if(sum == k){return true;}
+            else if(sum < k){left ++;
+            }
+            else {right--;}
+
+        }
+        return false;
+    }
+}
