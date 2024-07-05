0abc1731691d56beca3bc3c73199b8e8a89a1e3d
Ace <152076508+AkshayDilipYadav@users.noreply.github.com>
2024-07-06 01:35:57 +0530

    Add files via upload

---
+++ 24 07 06 MATHS GCD of A and B O log min of A or B O 1/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int result = GCD(a,b);
+        System.out.print(result);
+    }
+    public static int GCD(int a, int b){
+        while(b != 0){
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
